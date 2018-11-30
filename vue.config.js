0ac67f0a2aa57da19dc43cf30c88d3fc44f1f53e
liuhaowen <lhw@eie.ltd>
2018-11-30 11:26:33 +0800

    change build file

--- vue.config.js
+++ vue.config.js
@@ -1,120 +1,62 @@
+var path = require('path')
+// vue.config.js 配置说明
+//官方vue.config.js 参考文档 https://cli.vuejs.org/zh/config/#css-loaderoptions
+// 这里只列一部分，具体配置参考文档
 module.exports = {
-  // baseUrl: process.env.NODE_ENV === 'production' ?
-  //   '//your_url' : '/',
-  baseUrl: '/',
-
-  outputDir: 'docs',
-
-  assetsDir: 'static',
-
-  filenameHashing: true,
-
-  // When building in multi-pages mode, the webpack config will contain different plugins
-  // (there will be multiple instances of html-webpack-plugin and preload-webpack-plugin).
-  // Make sure to run vue inspect if you are trying to modify the options for those plugins.
-  // pages: {
-    // index: {
-      // // entry for the pages
-      // entry: 'src/pages/index/index.js',
-      // // the source template
-      // template: 'src/pages/index/index.html',
-      // // output as dist/index.html
-      // filename: 'index.html',
-      // // when using title option,
-      // // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
-      // title: '首页',
-      // // chunks to include on this pages, by default includes
-      // // extracted common chunks and vendor chunks.
-      // chunks: ['chunk-vendors', 'chunk-common', 'index']
-    // }
-    // // when using the entry-only string format,
-    // // template is inferred to be `public/subpage.html`
-    // // and falls back to `public/index.html` if not found.
-    // // Output filename is inferred to be `subpage.html`.
-    // // subpage: ''
-  // },
-
-  // eslint-loader 是否在保存的时候检查
+  // 部署生产环境和开发环境下的URL。
+  // 默认情况下，Vue CLI 会假设你的应用是被部署在一个域名的根路径上
+  //例如 https://www.my-app.com/。如果应用被部署在一个子路径上，你就需要用这个选项指定这个子路径。例如，如果你的应用被部署在 https://www.my-app.com/my-app/，则设置 baseUrl 为 /my-app/。
+  baseUrl: process.env.NODE_ENV === "production" ? "./" : "/",
+ 
+  // outputDir: 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）
+  outputDir: "docs",
+  //用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
+  assetsDir: "",
+  //指定生成的 index.html 的输出路径  (打包之后，改变系统默认的index.html的文件名)
+  // indexPath: "myIndex.html",
+  //默认情况下，生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存。你可以通过将这个选项设为 false 来关闭文件名哈希。(false的时候就是让原来的文件名不改变)
+  filenameHashing: false,
+ 
+  //   lintOnSave：{ type:Boolean default:true } 问你是否使用eslint
   lintOnSave: true,
-
-  // 是否使用包含运行时编译器的Vue核心的构建
-  runtimeCompiler: false,
-
-  // 默认情况下 babel-loader 忽略其中的所有文件 node_modules
-  transpileDependencies: [],
-
-  // 生产环境 sourceMap
+  //如果你想要在生产构建时禁用 eslint-loader，你可以用如下配置
+  // lintOnSave: process.env.NODE_ENV !== 'production',
+ 
+  //是否使用包含运行时编译器的 Vue 构建版本。设置为 true 后你就可以在 Vue 组件中使用 template 选项了，但是这会让你的应用额外增加 10kb 左右。(默认false)
+  // runtimeCompiler: false,
+ 
+  /**
+   * 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+   *  打包之后发现map文件过大，项目文件体积很大，设置为false就可以不输出map文件
+   *  map文件的作用在于：项目打包后，代码都是经过压缩加密的，如果运行时报错，输出的错误信息无法准确得知是哪里的代码报错。
+   *  有了map就可以像未加密的代码一样，准确的输出是哪一行哪一列有错。
+   * */
   productionSourceMap: false,
 
-  // cors 相关 https://jakearchibald.com/2017/es-modules-in-browsers/#always-cors
-  // corsUseCredentials: false,
-  // webpack 配置，键值对象时会合并配置，为方法时会改写配置
-  // https://cli.vuejs.org/guide/webpack.html#simple-configuration
-  configureWebpack: (config) => {},
-
-  // webpack 链接 API，用于生成和修改 webapck 配置
-  // https://github.com/mozilla-neutrino/webpack-chain
-  chainWebpack: (config) => {
-    // 因为是多页面，所以取消 chunks，每个页面只对应一个单独的 JS / CSS
-    config.optimization
-      .splitChunks({
-        cacheGroups: {}
-      });
-
-    // 'src/lib' 目录下为外部库文件，不参与 eslint 检测
-    config.module
-      .rule('eslint')
-      .exclude
-      .add('/Users/maybexia/Downloads/FE/community_built-in/src/lib')
-      .end()
-  },
-
-  // 配置高于chainWebpack中关于 css loader 的配置
-  css: {
-    // 是否开启支持 foo.module.css 样式
-    modules: false,
-
-    // 是否使用 css 分离插件 ExtractTextPlugin，采用独立样式文件载入，不采用 <style> 方式内联至 html 文件中
-    extract: true,
-
-    // 是否构建样式地图，false 将提高构建速度
-    sourceMap: false,
-
-    // css预设器配置项
-    loaderOptions: {
-      css: {
-        // options here will be passed to css-loader
-      },
-
-      postcss: {
-        // options here will be passed to postcss-loader
-      }
+  configureWebpack: {
+    output: {
+      
     }
   },
-
-  // All options for webpack-dev-server are supported
-  // https://webpack.js.org/configuration/dev-server/
+ 
+  // 它支持webPack-dev-server的所有选项
   devServer: {
-    open: true,
-
-    host: '127.0.0.1',
-
-    port: 3000,
-
-    https: false,
-
-    hotOnly: false,
-
-    proxy: null,
-
-    before: app => {}
-  },
-  // 构建时开启多进程处理 babel 编译
-  parallel: require('os').cpus().length > 1,
-
-  // https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa
-  pwa: {},
-
-  // 第三方插件配置
-  pluginOptions: {}
+    host: "localhost",
+    port: 8099, // 端口号
+    https: false, // https:{type:Boolean}
+    open: false, //配置自动启动浏览器
+    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
+ 
+    // 配置多个代理
+    // proxy: {
+    //   "/api": {
+    //     target: "<url>",
+    //     ws: true,
+    //     changeOrigin: true
+    //   },
+    //   "/foo": {
+    //     target: "<other_url>"
+    //   }
+    // }
+  }
 }
